7f7b0eaf8f8798272426c4185683c8b60c4344d9
niha009 <122668080+niha009@users.noreply.github.com>
2024-05-11 11:49:25 -0500

    Update settings.py

--- OnlineShopping/settings.py
+++ OnlineShopping/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'ujsx7y@d*q^b)ta6v6=v!3)ut4e0-zsg6xtlfp3qirfp5$#(3q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.elasticbeanstalk.com']
 
 
 # Application definition
